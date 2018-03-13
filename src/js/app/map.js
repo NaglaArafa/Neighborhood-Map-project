1e7218abe9673b021e20db909527f695d9e91bbf
Naglaa Arafa <naglaa.arafa@itworx.com>
2018-03-13 10:27:59 +0200

    call location.json after google map is loaded

--- src/js/app/map.js
+++ src/js/app/map.js
@@ -1,21 +1,36 @@
 var map,
     markers = [], // Create an array for all the markers.
     locations = [], // Create an array for all the Locations.
-    largeInfowindow;
+    largeInfowindow,
+    self = this;
+
 
 //loading data from json file
-$.getJSON("./locations.json", function (data) {
-    ko.utils.arrayForEach(data, function (item) {
-        locations.push(item);
+self.jsonCall = function() {
+    $.getJSON("./locations.json", function(data) {
+        ko.utils.arrayForEach(data, function(item) {
+            locations.push(item);
+        });
+    }).done(function() {
+        ko.applyBindings(new FilterList(locations));
+        for (var i = 0; i < locations.length; i++) {
+            // Get the position from the location array.
+            var position = locations[i].location;
+            var title = locations[i].title;
+            var id = locations[i].id;
+            var venueID = locations[i].venueID;
+
+            // Create a marker per location, and put into markers array.
+            self.setMarker(position, title, id, venueID);
+        }
+    }).fail(function() {
+        alert("Can't load locations, Please refresh to try again.");
     });
-}).done(function () {
-    ko.applyBindings(new FilterList(locations));
-}).fail(function (error) {
-    alert("Can't load locations, Please refresh to try again.")
-});
+};
+
 
 //create new marker
-this.setMarker = function (position, title, id, venueID) {
+self.setMarker = function(position, title, id, venueID) {
     this.marker = new google.maps.Marker({
         map: map,
         position: position,
@@ -25,31 +40,31 @@ this.setMarker = function (position, title, id, venueID) {
         venueID: venueID
     });
     markers.push(this.marker);
-    this.marker.addListener('click', openInfoWindow)
+    this.marker.addListener('click', openInfoWindow);
 }
 
 //function that handle click on listItem or marker
-this.openInfoWindow = function () {
-    populateInfoWindow(markers[this.id], largeInfowindow)
+self.openInfoWindow = function() {
+    populateInfoWindow(markers[this.id], largeInfowindow);
 }
 
 // This function populates the infowindow when the marker is clicked. We'll only allow
 // one infowindow which will open at the marker that is clicked, and populate based
 // on that markers position.
-this.populateInfoWindow = function (marker, infowindow) {
+self.populateInfoWindow = function(marker, infowindow) {
     // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
         infowindow.marker = marker;
         infowindow.marker.setAnimation(google.maps.Animation.BOUNCE);
         map.panTo(infowindow.marker.position);
-        callFourSquareApi(marker.venueID, infowindow);
+        self.callFourSquareApi(marker.venueID, infowindow);
         map.setZoom(13);
         infowindow.open(map, marker);
-        setTimeout(function () {
+        setTimeout(function() {
             infowindow.marker.setAnimation(null);
         }, 1400);
         // Make sure the marker property is cleared if the infowindow is closed.
-        infowindow.addListener('closeclick', function () {
+        infowindow.addListener('closeclick', function() {
             infowindow.marker = null;
             map.setZoom(11);
         });
@@ -57,32 +72,31 @@ this.populateInfoWindow = function (marker, infowindow) {
 }
 
 //FourSquare Api Function
-function callFourSquareApi(venueID, infowindow) {
+self.callFourSquareApi = function(venueID, infowindow) {
     var ClientID = 'GKFSFL01K4A3QNPFWPBRALYXPA4OSKVWYAAAOW441TY3P2BR',
         ClientSecret = 'Z1ZKVHZQPJUHW0TR1FT0QFASMPIT0KMZIZSVZ3IRAMFVAEU0',
         URL = 'https://api.foursquare.com/v2/venues/' + venueID + '?client_id=' + ClientID + '&client_secret=' + ClientSecret + '&v=20180312';
 
-    $.getJSON(URL).done(function (data) {
-        console.log(data.response.venue);
+    $.getJSON(URL).done(function(data) {
         var response = data.response.venue || '',
             name = response.name || '',
             phone = response.contact.phone || '',
             description = response.description || '',
             url = response.url || '',
-            crossStreet=response.location.crossStreet ||'',
-            address=response.location.address||'',
-            city=response.location.city || ''
-            country=response.location.country || '',
-            fullAddress = crossStreet+ ', ' +address  + ', ' + city  + ', ' + country;
+            crossStreet = response.location.crossStreet || '',
+            address = response.location.address || '',
+            city = response.location.city || '',
+            country = response.location.country || '',
+            fullAddress = crossStreet + ', ' + address + ', ' + city + ', ' + country;
 
         infowindow.setContent('<div>' + name + '</div><div>' + phone + '</div><div>' + description + '</div><div>' + url + '</div><div>' + fullAddress + '</div>');
-    }).fail(function () {
+    }).fail(function() {
         alert("There was an error, Data can't be loaded. Please refresh the page to try again.");
     });
 }
 
 //initial function that loaded after map loaded
-this.initMap = function () {
+self.initMap = function() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 30.0518051, lng: 31.4022244 },
@@ -90,18 +104,5 @@ this.initMap = function () {
     });
 
     largeInfowindow = new google.maps.InfoWindow();
-
-    for (var i = 0; i < locations.length; i++) {
-        // Get the position from the location array.
-        var position = locations[i].location;
-        var title = locations[i].title;
-        var id = locations[i].id;
-        var venueID = locations[i].venueID;
-
-        // Create a marker per location, and put into markers array.
-        this.setMarker(position, title, id, venueID)
-    }
+    self.jsonCall();
 }
-
-
-
